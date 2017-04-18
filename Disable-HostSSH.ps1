44f179cebd71e2d259736abb038a5ff13268cd67
Nick Hill <ndhill@gmail.com>
2017-04-18 15:03:58 -0700

    Add files via upload

---
+++ Disable-HostSSH.ps1
@@ -0,0 +1,17 @@
+
+Do{
+$Do = Read-Host -Prompt "Turn off SSH on all hosts? (Y/N)"
+} Until (($Do -match "Y") -or ($Do -match "N") -or ($Do -match "y") -or ($Do -match "n"))
+    
+
+if($Do -match "N"){
+    Write-Host "Quitting..."
+    
+} 
+if ($Do -Match "Y") {
+    
+    Write-Host "Doing Something"
+    Get-VMHost | ForEach {Stop-VMHostService -HostService ($_ | Get-VMHostService | Where {$_.Key -eq “TSM-SSH”})}
+
+}
+    
