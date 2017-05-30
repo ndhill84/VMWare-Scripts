7f6a7928af286964103b293f389d77eef129b66e
Nick Hill <ndhill@gmail.com>
2017-05-30 12:11:11 -0700

    Added Try-Catch to Get-VM command

--- Fix-PublicCloudNic.ps1
+++ Fix-PublicCloudNic.ps1
@@ -5,7 +5,13 @@ if (!$global:DefaultVIServer)
     }
 
 $vmName = Read-Host "VM Name"
-$vm = get-vm -name $vmName
+try {
+    $vm = get-vm -name $vmName
+}
+catch {
+    write-host "Couldn't find VM" $vm "Check the name."
+	exit
+}
 $nic = $vm | Get-NetworkAdapter
 
 Set-NetworkAdapter -NetworkAdapter $nic -Connected:$false
